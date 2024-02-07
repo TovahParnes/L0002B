df6b4dcd542c996e8a15dac7ba2e7ceda889f1e2
Tovah Parnes <tovah@parnes.com>
2024-02-07 11:41:03 +0100

    completed assignemnt 1 forms

---
+++ Assignment1/WindowsFormsApp/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿// Tovah Parnes, tovah@parnes.com, L0002B, Uppgift 1 Forms-variant
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.tbPrice = new System.Windows.Forms.TextBox();
+            this.tbPaid = new System.Windows.Forms.TextBox();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblPaid = new System.Windows.Forms.Label();
+            this.lblChange = new System.Windows.Forms.Label();
+            this.btnExitProgram = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(412, 99);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(183, 43);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Kalkulera växel";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Location = new System.Drawing.Point(279, 174);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(0, 20);
+            this.lblHelloWorld.TabIndex = 1;
+            // 
+            // tbPrice
+            // 
+            this.tbPrice.Location = new System.Drawing.Point(52, 68);
+            this.tbPrice.Name = "tbPrice";
+            this.tbPrice.Size = new System.Drawing.Size(230, 26);
+            this.tbPrice.TabIndex = 2;
+            this.tbPrice.TextChanged += new System.EventHandler(this.tbPrice_TextChanged);
+            // 
+            // tbPaid
+            // 
+            this.tbPaid.Location = new System.Drawing.Point(52, 145);
+            this.tbPaid.Name = "tbPaid";
+            this.tbPaid.Size = new System.Drawing.Size(230, 26);
+            this.tbPaid.TabIndex = 3;
+            this.tbPaid.TextChanged += new System.EventHandler(this.tbPaid_TextChanged);
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(48, 40);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(80, 20);
+            this.lblPrice.TabIndex = 4;
+            this.lblPrice.Text = "Ange pris:";
+            this.lblPrice.Click += new System.EventHandler(this.lblPrice_Click);
+            // 
+            // lblPaid
+            // 
+            this.lblPaid.AutoSize = true;
+            this.lblPaid.Location = new System.Drawing.Point(48, 122);
+            this.lblPaid.Name = "lblPaid";
+            this.lblPaid.Size = new System.Drawing.Size(104, 20);
+            this.lblPaid.TabIndex = 5;
+            this.lblPaid.Text = "Ange betalat:";
+            this.lblPaid.Click += new System.EventHandler(this.lblPaid_Click);
+            // 
+            // lblChange
+            // 
+            this.lblChange.AutoSize = true;
+            this.lblChange.Location = new System.Drawing.Point(48, 207);
+            this.lblChange.Name = "lblChange";
+            this.lblChange.Size = new System.Drawing.Size(105, 20);
+            this.lblChange.TabIndex = 6;
+            this.lblChange.Text = "Växel tillbaka:";
+            this.lblChange.Click += new System.EventHandler(this.lblChange_Click);
+            // 
+            // btnExitProgram
+            // 
+            this.btnExitProgram.BackColor = System.Drawing.Color.Brown;
+            this.btnExitProgram.Location = new System.Drawing.Point(646, 12);
+            this.btnExitProgram.Name = "btnExitProgram";
+            this.btnExitProgram.Size = new System.Drawing.Size(142, 43);
+            this.btnExitProgram.TabIndex = 7;
+            this.btnExitProgram.Text = "Stäng program";
+            this.btnExitProgram.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExitProgram.UseVisualStyleBackColor = false;
+            this.btnExitProgram.Click += new System.EventHandler(this.btnExitProgram_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExitProgram);
+            this.Controls.Add(this.lblChange);
+            this.Controls.Add(this.lblPaid);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.tbPaid);
+            this.Controls.Add(this.tbPrice);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.TextBox tbPrice;
+        private System.Windows.Forms.TextBox tbPaid;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblPaid;
+        private System.Windows.Forms.Label lblChange;
+        private System.Windows.Forms.Button btnExitProgram;
+    }
+}
+
---
+++ Assignment1/WindowsFormsApp/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,87 @@
+﻿// Tovah Parnes, tovah@parnes.com, L0002B, Uppgift 1 Forms-variant
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string output;
+        int[] values = { 500, 200, 100, 50, 20, 10, 5, 1 };
+        string[] valueNames = { "500-lappar", "200-lappar", "100-lappar", "50-lappar", "20-lappar", "10-kronor", "5-kronor", "1-kronor" };
+
+
+
+        private void tbPrice_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPrice_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPaid_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbPaid_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblChange_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            output = "";
+            int price = int.Parse(tbPrice.Text);
+            int paid = int.Parse(tbPaid.Text);
+
+            int change = paid - price;
+            output = output + "Växel tillbaka: " + change + " kr\n";
+
+            for (int i = 0; i<values.Length; i++) {
+            change = calculateChange(change, values[i], valueNames[i]);
+            }
+
+            lblChange.Text = output;
+        }
+
+        private int calculateChange(int change, int value, string valueName)
+        {
+            int valAmmount = (int)Math.Floor((decimal)change / value);
+            if (valAmmount > 0)
+            {
+                output = output + valueName + ": " + valAmmount + "\n";
+                change = change % value;
+            }
+            return change;
+        }
+
+        private void btnExitProgram_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
