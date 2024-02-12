31349369d42173964612875312ced90526e0fa8f
TovahParnes <tovah@parnes.com>
2024-02-12 15:18:18 +0100

    broke the program when trying to add write to file

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,18 +1,17 @@
 ﻿// Tovah Parnes, tovah@parnes.com, L0002B, Uppgift 2 Console-variant
-using System;
+using System; 
+using System.Collections.Generic;
 Console.WriteLine("Tovah Parnes, tovah@parnes.com, L0002B, Uppgift 2 Console-variant");
 
 List<Person> persons = new List<Person>();
-persons.Add(new Person("Kalle Anka", "4503038990", "Piteå", 173));
-persons.Add(new Person("Snobben", "7805055673", "Luleå", 203));
-persons.Add(new Person("Musse Pigg", "3502038964", "Boden", 202));
 
 while (true) {
     Console.WriteLine();
     Console.WriteLine("Menu:");
     Console.WriteLine("1. Input person");
     Console.WriteLine("2. Print list");
-    Console.WriteLine("3. Exit");
+    Console.WriteLine("3. Add list of people");
+    Console.WriteLine("4. Exit");
     int choice = int.Parse(Console.ReadLine());
     Console.WriteLine();
 
@@ -23,10 +22,22 @@ while (true) {
         persons = bucketSortPeople(persons);
         printPeople(persons);
     } else if (choice == 3) {
+        persons.Add(new Person("Kalle Anka", "4503038990", "Piteå", 173));
+        persons.Add(new Person("Snobben", "7805055673", "Luleå", 203));
+        persons.Add(new Person("Musse Pigg", "3502038964", "Boden", 202));
+        persons.Add(new Person("Musse Pigg", "3502038964", "Boden", 2));
+        persons.Add(new Person("Musse Pigg", "3502038964", "Boden", 222));
+        persons.Add(new Person("Musse Pigg", "3502038964", "Boden", 302));
+        persons.Add(new Person("Musse Pigg", "3502038964", "Boden", 22));
+        persons.Add(new Person("Musse Pigg", "3502038964", "Boden", 72));
+        persons.Add(new Person("Musse Pigg", "3502038964", "Boden", 82));
+        persons.Add(new Person("Musse Pigg", "3502038964", "Boden", 50));
+    } else if (choice == 4) {
         Environment.Exit(0);
     }
 }  
 
+//Reads the input from the console and returns a person
 static Person InputPerson() {
     Console.WriteLine("Namn:");
     string name = Console.ReadLine();
@@ -43,6 +54,7 @@ static Person InputPerson() {
     return new Person(name, pnum, district, sold);
 }
 
+//Sorts the list into 4 buckeds (based on the different levels), each bucket is sorted and combined together
 static List<Person> bucketSortPeople(List<Person> persons) {
     List<Person> level0_49 = new List<Person>();
     List<Person> leve50_99 = new List<Person>();
@@ -71,39 +83,53 @@ static List<Person> bucketSortPeople(List<Person> persons) {
     return sortedPeople;
 }
 
+//Bubblesorts all of the people of a given list, only one bucket is sorted at a time
 static List<Person> bubbleSortPeople(List<Person> persons) {
-
+    var n = persons.Count;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (persons[j].Sold > persons[j + 1].Sold)
+            {
+                var tempVar = persons[j];
+                persons[j] = persons[j + 1];
+                persons[j + 1] = tempVar;
+            }
     return persons;
 }
 
+//Prints people to the console and a file
 static void printPeople(List<Person> persons) {
-    Console.WriteLine("{0,15} {1,15} {2,10} {3,10}", "Namn", "Pers.Num", "Distrikt", "Antal sålda");  
+    //Console.WriteLine("{0,15} {1,15} {2,10} {3,10}", "Namn", "Pers.Num", "Distrikt", "Antal sålda");  
     int oldSold = 0;
     int count = 0;
+    List<string> toPrint = new List<string>();
+    toPrint.Add(("{0,15} {1,15} {2,10} {3,10}", "Namn", "Pers.Num", "Distrikt", "Antal sålda"));
     foreach (Person person in persons) {
-        //Console.WriteLine("-------- oldSold: " + oldSold + "currentSold: " + person.Sold);
         if (oldSold < 50 && person.Sold >= 50) {
-            printLevel(count, "0-49");
+            Console.WriteLine(strLevel(count, "0-49"));
             count = 0;
         } else if (oldSold < 100 && person.Sold >= 100) {
-            printLevel(count, "50-99");
+            Console.WriteLine(strLevel(count, "50-99"));
             count = 0;
         } else if (oldSold < 200 && person.Sold >= 200) {
-            printLevel(count, "100-199");
+            Console.WriteLine(strLevel(count, "100-199"));
             count = 0;
 
         }
         count++;
-        Console.WriteLine("{0,15} {1,15} {2,10} {3,10}", person.Name, person.Pnum, person.District, person.Sold);
+        toPrint += ("{0,15} {1,15} {2,10} {3,10}", person.Name, person.Pnum, person.District, person.Sold);
         oldSold = person.Sold;
     }
-    printLevel(count, "200+");
+    Console.WriteLine(strLevel(count, "200+"));
+    Console.WriteLine(toPrint);
 }
 
-static void printLevel(int count, string lvl) {
+
+static string strLevel(int count, string lvl) {
     if (count != 0) {
-        Console.WriteLine(count + " säljare har nått nivå " + lvl + " artiklar\n");
+        return(count + " säljare har nått nivå " + lvl + " artiklar\n");
     }
+    return "";
 }
 
 public class Person(string name, string pnum, string district, int sold){
